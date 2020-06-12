6d064ecd9f3725ad9264b988e84ebeccc0dcae23
NelfM <nealmalhotra2006@gmail.com>
2020-06-12 00:13:05 -0700

    Initial Commit

---
+++ vidur/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vidur
+//
+//  Created by Neal Malhotra on 6/12/20.
+//  Copyright © 2020 Neal Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
